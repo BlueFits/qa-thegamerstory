3a7552130bdb5bc2ee84cf093f4198c7746d7631
Christian.Rojas@coxautoinc.com <Christian.Rojas@coxautoinc.com>
2021-08-11 11:09:46 -0400

    changed server config

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const path = require('path');
+
+    const indexRouter = require('./routes/index');
+    const server = express();
+
+    server.use("/api", indexRouter);
+
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
