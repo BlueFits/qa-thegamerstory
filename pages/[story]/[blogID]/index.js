6c0b1e8999d76db69658b9f50aa0f7a136a1cab9
Christian.Rojas@coxautoinc.com <Christian.Rojas@coxautoinc.com>
2021-08-13 15:58:34 -0400

    conformed to eslint

--- pages/[story]/[blogID]/index.js
+++ pages/[story]/[blogID]/index.js
@@ -1,6 +1,8 @@
 import Banner from "../../../components/Banner/Banner";
 import Layout from "../../../components/Layout/Layout";
 import Typography from "../../../components/Typography/Typography";
+import Image from "next/image";
+import styles from "./blogID.module.css";
 
 const Index = () => {
     return (
@@ -17,19 +19,19 @@ const Index = () => {
                     </div>
                     <Typography type="r2">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                         It has survived not only five centuries, but also the leap into electronic typesetting, 
                         remaining essentially unchanged. It was popularised in the 1960s with the release of 
                         Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
                         software like Aldus PageMaker including versions of Lorem Ipsum.
                     </Typography>
-
-                    <img style={{ width: "100%", margin: "100px 0" }} src="https://media.discordapp.net/attachments/872578374256717854/873749248842285096/Final_Fantasy_XIV_A_Realm_Reborn_Screenshot_2021.08.07_-_22.06.57.15.png?width=1101&height=461"/>
+                    
+                    <div className={styles.blogImage} style={{ backgroundImage: `url(https://media.discordapp.net/attachments/872578374256717854/873749248842285096/Final_Fantasy_XIV_A_Realm_Reborn_Screenshot_2021.08.07_-_22.06.57.15.png?width=1101&height=461)`}} />
 
                     <Typography type="r2">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, 
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                         It has survived not only five centuries, but also the leap into electronic typesetting, 
                         remaining essentially unchanged. It was popularised in the 1960s with the release of 
